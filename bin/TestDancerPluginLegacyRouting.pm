c0e33bfd1c63350bce0e12ddb84376eb88083d86
Robert Stone <robertstone@hostgator.com>
2014-01-30 15:30:00 -0600

    Fully implemented legacy_get with tests.
    In bin/TestDancerPluginLegacyRouting:
      Added routes for testing with /:var and GET params
    
    In t/01-legacy_get.t
      Added Tests for Cases:
        Legacy Route Called NOT Logged when Logging NOT Enabled
        Legacy Route with URI Parameter
        Legacy Route with GET Args

--- bin/TestDancerPluginLegacyRouting.pm
+++ bin/TestDancerPluginLegacyRouting.pm
@@ -10,9 +10,30 @@ use Dancer::Plugin::Legacy::Routing;
 get        "/good/get"   => \&test_get;
 legacy_get "/legacy/get" => \&test_get;
 
+get        "/good/get/:var"   => \&test_get_with_var;
+legacy_get "/legacy/get/:var" => \&test_get_with_var;
+
+get        "/good/get/:var/params"   => \&test_get_with_params;
+legacy_get "/legacy/get/:var/params" => \&test_get_with_params;
+
 sub test_get {
     status 200;
     return "Testing Get";
 }
 
+sub test_get_with_var {
+    my $var = params->{'var'};
+
+    status 200;
+    return "Testing Get, Var Value is = " . $var;
+}
+
+sub test_get_with_params {
+    my $var1 = params->{'var1'};
+    my $var2 = params->{'var2'};
+
+    status 200;
+    return "Testing Get, Var1 Value is = " . $var1
+        . " Var2 Value is = " . $var2;
+}
 1;
